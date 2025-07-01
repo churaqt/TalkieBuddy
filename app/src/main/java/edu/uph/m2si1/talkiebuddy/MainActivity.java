e22ea75164836d875eaedffb0e5d6e905b201708
Callista1409 <callista1405@gmail.com>
2025-07-01 21:25:49 +0700

    Penambahan Kode untuk Fragment

--- app/src/main/java/edu/uph/m2si1/talkiebuddy/MainActivity.java
+++ app/src/main/java/edu/uph/m2si1/talkiebuddy/MainActivity.java
@@ -1,24 +1,43 @@
 package edu.uph.m2si1.talkiebuddy;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import edu.uph.m2si1.talkiebuddy.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Fragment default saat pertama dibuka
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.navHome:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.navProfil:
+                    replaceFragment(new ProfilFragment());
+                    break;
+                case R.id.navTips:
+                    replaceFragment(new TipsFragment());
+                    break;
+            }
+            return true;
         });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, fragment)
+                .commit();
     }
 }
