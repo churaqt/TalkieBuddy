6b1f7554b77b240b6d0417624285f36090482604
churaqt <desintayamasanr@gmail.com>
2025-07-01 18:07:07 +0700

    update baru bottom nav ngga muncul (dess)

--- app/src/main/java/edu/uph/m2si1/talkiebuddy/DashboardActivity.java
+++ app/src/main/java/edu/uph/m2si1/talkiebuddy/DashboardActivity.java
@@ -1,34 +1,34 @@
-package edu.uph.m2si1.talkiebuddy;
+package edu.uph.m2si1.talkiebuddy.ui;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import edu.uph.m2si1.talkiebuddy.databinding.ActivityDashboardBinding;
+import edu.uph.m2si1.talkiebuddy.R;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    private ActivityDashboardBinding binding;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard); // layout XML yg ada BottomNavigationView-nya
 
-        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        BottomNavigationView navView = binding.navView;
+        // Set 3 fragment sebagai top-level destination
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_tips)
+                .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_dashboard);
+
         NavigationUI.setupWithNavController(navView, navController);
-
-
     }
-
 }
--- app/src/main/java/edu/uph/m2si1/talkiebuddy/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/edu/uph/m2si1/talkiebuddy/ui/dashboard/DashboardFragment.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import edu.uph.m2si1.talkiebuddy.databinding.FragmentDashboardBinding;
 
@@ -18,15 +16,10 @@ public class DashboardFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
+        // Inflate layout dengan View Binding
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot(); // langsung return root layout
     }
 
     @Override
