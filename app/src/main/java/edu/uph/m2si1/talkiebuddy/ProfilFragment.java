14b85f511c07fe0c82964ac20fc07e588ad7b0c3
Veligho12 <velicia1267@gmail.com>
2025-07-09 15:45:26 +0700

    Update ProfilFragment.java
    Set onclickListener untuk fitur swipe setting panel

--- app/src/main/java/edu/uph/m2si1/talkiebuddy/ProfilFragment.java
+++ app/src/main/java/edu/uph/m2si1/talkiebuddy/ProfilFragment.java
@@ -1,42 +1,57 @@
 package edu.uph.m2si1.talkiebuddy;
 
 import android.os.Bundle;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import androidx.core.view.GravityCompat;
 
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import edu.uph.m2si1.talkiebuddy.adapter.ProgressNoteAdapter;
-import edu.uph.m2si1.talkiebuddy.model.ProgressNote;
-import io.realm.Realm;
-import io.realm.RealmResults;
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProfilFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class ProfilFragment extends Fragment {
 
-    private RecyclerView rvProgress;
-    private Button btnAddProgress;
-    private ProgressNoteAdapter adapter;
-    private List<ProgressNote> noteList = new ArrayList<>();
-    private Realm realm;
+    DrawerLayout drawerLayout;
+    ImageView imgSetting;
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     public ProfilFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProfilFragment.
+     */
+    // TODO: Rename and change types and number of parameters
     public static ProfilFragment newInstance(String param1, String param2) {
         ProfilFragment fragment = new ProfilFragment();
         Bundle args = new Bundle();
-        args.putString("ARG_PARAM1", param1);
-        args.putString("ARG_PARAM2", param2);
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -44,13 +59,16 @@ public class ProfilFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Realm.init(requireContext()); // init Realm
-        realm = Realm.getDefaultInstance();
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profil, container, false);
     }
 
@@ -58,33 +76,20 @@ public class ProfilFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        rvProgress = view.findViewById(R.id.rvProgress);
-        btnAddProgress = view.findViewById(R.id.btnAddProgress);
+        // Inisialisasi view
+        drawerLayout = view.findViewById(R.id.drawer_layout);
+        imgSetting = view.findViewById(R.id.imgSetting);
 
-        rvProgress.setLayoutManager(new LinearLayoutManager(requireContext()));
-        loadNotes(); // load from Realm
-
-        btnAddProgress.setOnClickListener(v -> {
-            AddNoteDialog dialog = new AddNoteDialog();
-            dialog.setNoteSavedListener(this::loadNotes); // callback ke loadNotes()
-            dialog.show(getParentFragmentManager(), "AddNoteDialog");
+        // Listener untuk buka/tutup drawer
+        imgSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                    drawerLayout.closeDrawer(GravityCompat.END);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.END);
+                }
+            }
         });
-
-    }
-
-    private void loadNotes() {
-        RealmResults<ProgressNote> results = realm.where(ProgressNote.class).findAll();
-        noteList.clear();
-        noteList.addAll(results);
-        adapter = new ProgressNoteAdapter(noteList);
-        rvProgress.setAdapter(adapter);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (realm != null && !realm.isClosed()) {
-            realm.close();
-        }
     }
 }
