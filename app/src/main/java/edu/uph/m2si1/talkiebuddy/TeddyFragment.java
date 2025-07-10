ba0b4db486a873596cb092a13db0f0a8207b1d63
churaqt <desintayamasanr@gmail.com>
2025-07-10 19:27:57 +0700

    test teddy.. -dess

--- app/src/main/java/edu/uph/m2si1/talkiebuddy/TeddyFragment.java
+++ app/src/main/java/edu/uph/m2si1/talkiebuddy/TeddyFragment.java
@@ -11,12 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class TeddyFragment extends Fragment {
@@ -32,13 +30,8 @@ public class TeddyFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public static TeddyFragment newInstance(String param1, String param2) {
-        TeddyFragment fragment = new TeddyFragment();
-        Bundle args = new Bundle();
-        args.putString("param1", param1);
-        args.putString("param2", param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static TeddyFragment newInstance() {
+        return new TeddyFragment();
     }
 
     @Override
@@ -46,27 +39,30 @@ public class TeddyFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_teddy, container, false);
 
-        // Binding UI components
+        // Bind all view components
         edtName = view.findViewById(R.id.edtName);
         edtBirthday = view.findViewById(R.id.edtBirthday);
         edtPersonality = view.findViewById(R.id.edtPersonality);
         edtTriggerSound = view.findViewById(R.id.edtTriggerSound);
         seekBarVoiceSpeed = view.findViewById(R.id.seekBarVoiceSpeed);
         spinnerEmotion = view.findViewById(R.id.spinnerEmotion);
+
         radioGroupWordLevel = view.findViewById(R.id.radioGroupWordLevel);
-        radioGroupRepeat = view.findViewById(R.id.radioGroupRepeat);
         radioEasy = view.findViewById(R.id.radioEasy);
         radioMedium = view.findViewById(R.id.radioMedium);
         radioHard = view.findViewById(R.id.radioHard);
+
+        radioGroupRepeat = view.findViewById(R.id.radioGroupRepeat);
         radio2 = view.findViewById(R.id.radio2);
         radio3 = view.findViewById(R.id.radio3);
         radio4 = view.findViewById(R.id.radio4);
+
         btnSave = view.findViewById(R.id.btnSave);
 
-        // Load saved data
+        // Load data
         loadSavedData();
 
-        // Save button click
+        // Button Save Click
         btnSave.setOnClickListener(v -> saveData());
 
         return view;
@@ -80,21 +76,34 @@ public class TeddyFragment extends Fragment {
         editor.putString("birthday", edtBirthday.getText().toString());
         editor.putString("personality", edtPersonality.getText().toString());
         editor.putInt("voiceSpeed", seekBarVoiceSpeed.getProgress());
-        editor.putString("emotion", spinnerEmotion.getSelectedItem().toString());
+
+        if (spinnerEmotion.getSelectedItem() != null) {
+            editor.putString("emotion", spinnerEmotion.getSelectedItem().toString());
+        }
+
         editor.putString("triggerSound", edtTriggerSound.getText().toString());
 
         int wordLevelId = radioGroupWordLevel.getCheckedRadioButtonId();
-        if (wordLevelId == R.id.radioEasy) editor.putString("wordLevel", "Easy");
-        else if (wordLevelId == R.id.radioMedium) editor.putString("wordLevel", "Medium");
-        else if (wordLevelId == R.id.radioHard) editor.putString("wordLevel", "Hard");
+        if (wordLevelId == R.id.radioEasy) {
+            editor.putString("wordLevel", "Easy");
+        } else if (wordLevelId == R.id.radioMedium) {
+            editor.putString("wordLevel", "Medium");
+        } else if (wordLevelId == R.id.radioHard) {
+            editor.putString("wordLevel", "Hard");
+        }
 
         int repeatId = radioGroupRepeat.getCheckedRadioButtonId();
-        if (repeatId == R.id.radio2) editor.putInt("repeat", 2);
-        else if (repeatId == R.id.radio3) editor.putInt("repeat", 3);
-        else if (repeatId == R.id.radio4) editor.putInt("repeat", 4);
+        if (repeatId == R.id.radio2) {
+            editor.putInt("repeat", 2);
+        } else if (repeatId == R.id.radio3) {
+            editor.putInt("repeat", 3);
+        } else if (repeatId == R.id.radio4) {
+            editor.putInt("repeat", 4);
+        }
 
         editor.apply();
-        Toast.makeText(requireContext(), "Saved!", Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(requireContext(), "Data berhasil disimpan!", Toast.LENGTH_SHORT).show();
     }
 
     private void loadSavedData() {
@@ -106,7 +115,7 @@ public class TeddyFragment extends Fragment {
         edtTriggerSound.setText(sharedPref.getString("triggerSound", ""));
         seekBarVoiceSpeed.setProgress(sharedPref.getInt("voiceSpeed", 0));
 
-        // Spinner Emotion
+        // Emotion Spinner
         String emotion = sharedPref.getString("emotion", "");
         for (int i = 0; i < spinnerEmotion.getCount(); i++) {
             if (spinnerEmotion.getItemAtPosition(i).toString().equals(emotion)) {
@@ -115,13 +124,13 @@ public class TeddyFragment extends Fragment {
             }
         }
 
-        // Word Level Radio
+        // Word Level
         String wordLevel = sharedPref.getString("wordLevel", "");
         if (wordLevel.equals("Easy")) radioEasy.setChecked(true);
         else if (wordLevel.equals("Medium")) radioMedium.setChecked(true);
         else if (wordLevel.equals("Hard")) radioHard.setChecked(true);
 
-        // Repeat Radio
+        // Repeat
         int repeat = sharedPref.getInt("repeat", 0);
         if (repeat == 2) radio2.setChecked(true);
         else if (repeat == 3) radio3.setChecked(true);
